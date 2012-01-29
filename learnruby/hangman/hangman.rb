2d65e26c4e8dcca7d31e5ff83d52e2548ba49dfa
Axel Auffarth <axela@tzi.de>
2012-01-29 19:10:05 +0100

    added american english dictionary this version works but only with linux tools wc and sed available ;)
    	modified:   ../d3ni/hangman.rb
    	new file:   american-english
    	modified:   hangman.rb

--- learnruby/hangman/hangman.rb
+++ learnruby/hangman/hangman.rb
@@ -1,20 +1,34 @@
 #!/usr/bin/env ruby
+n = Random.rand(1..`wc -l american-english`.to_i) # random line number
+hidden = `sed -ne '#{n} p' american-english`.chomp.downcase # get word with sed out of dict
+if hidden[-2] == 39.chr # == ' 
+ hidden=hidden[0...-2] #remove genitive 's like in elephant's
+end
 bads = 0
-hidden = "aortenaneurysma"
 left = hidden.length
 secret = "_" * hidden.length
+a = []
+#puts hidden #uncomment for better debugging
 while left > 0
  puts secret + " %d chars left, %i bads\n" % [left,bads]
  input = gets[0]
- i = 0
- hidden.each_char do |c|
-  if c == input 
-   secret[i] = input
-   left-=1
-  else 
+ if a.include?(input)
+  puts "you already had %s" % input
+ else
+  a += [input]
+  i = 0
+  found=false
+  hidden.each_char do |c|
+   if c == input 
+    secret[i] = input
+    found=true
+    left-=1
+   end   
+   i+=1
+  end
+  if !found 
    bads+=1
   end
-  i+=1
  end
 end
-puts "well done. you found %s," % [hidden]
+puts "well done. you found \"%s\" with %i bads" % [hidden,bads]
