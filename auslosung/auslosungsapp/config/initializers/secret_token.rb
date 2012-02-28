1ceb524f37ef21cd7dd9ddaef26d47a6fbdd78f9
Axel Auffarth <axela@tzi.org>
2012-02-28 15:00:31 +0100

    new rails 3.2.1 app

---
+++ auslosung/auslosungsapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auslosungsapp::Application.config.secret_token = '007523af328747bad684147ee994e9dd70e5ff2363da6e624350c5f755901540de87b89f2c0043d9c09dee0fb69eb07fee9d0509873ac5f7905aee0d127e9d36'
